af088282a7795176d0a775169d2c00eb99253473
yuripc7 <133939847+yuripc7@users.noreply.github.com>
2026-05-05 22:02:06 -0300

    feat: add lib/supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  'https://jhaaxyjmvcxfotkoalhq.supabase.co',
+  'sb_publishable_S7P8lzgH2Z5I6eCgBjZMhw_u9P1EKc_'
+);

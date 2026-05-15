7e5f981d7347a1b3635875c4c45a49412b13e58e
yuripc7 <133939847+yuripc7@users.noreply.github.com>
2026-05-15 20:54:02 -0300

    chore: update Supabase credentials to Enigami project

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://jhaaxyjmvcxfotkoalhq.supabase.co',
-  'sb_publishable_S7P8lzgH2Z5I6eCgBjZMhw_u9P1EKc_'
+  'https://qlvvpgfotcdtdypvkrvu.supabase.co',
+  'sb_publishable_s894em2YE-BXyabblSnemQ_4Jwx7AoB'
 );
